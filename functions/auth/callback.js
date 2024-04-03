c2df70e8fc073a37d1548749616a52eeeee8043d
orsanawwad <orsan.1997@gmail.com>
2024-04-03 08:50:29 +0300

    Debug log

--- functions/auth/callback.js
+++ functions/auth/callback.js
@@ -28,7 +28,8 @@ export async function onRequest(context) {
     if (responseData.access_token) {
       return Response.redirect(`${env.BASE_URL}/?access_token=${responseData.access_token}`, 302);
     } else {
-      throw new Error('Access token not found');
+      throw new Error(responseData);
+      // throw new Error('Access token not found');
     }
   }
 
